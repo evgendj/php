9f78f2bb13b2f7861bc0bdfe912d8ddf23b9af8a
evgendj <workinformer@gmail.com>
2019-12-11 10:08:11 +0300

    Пауза у Скляра, создан php в подлиннике

--- php-book.php
+++ php-book.php
@@ -34,6 +34,54 @@ TABLE;
 
 ***************************ООП****************************
 
+class Entree {	// определение класса
+	public $name;	// свойство
+	public $ingredients = array();	// другое свойство
+	public function hasIngredient($ingredient) {	// метод
+		return in_array($ingredient, $this->ingredients);	// $this - спец. переменная, ссылающаяся на тот экземпляр класса, из которого вызывается метод.
+	}
+
+	public static function getSizes() {	// Объявление статического метода
+		return array('small', 'medium', 'large');
+	}
+}
+// создание экземпляра класса и присвоение его переменной
+$soup = new Entree; // new возвращает новы объект типа Entree
+$soup->name = 'Chicken Soup';	// установка значений свойств экзепляра в переменной $soup
+$soup->ingredients = ['chicken', 'water'];
+// еще один экземпляр
+$sandwich = new Entree;
+$sandwich->name = 'Chicken Sandwich';
+$sandwich->ingredients = ['chicken', 'bread'];
+
+foreach (['chicken', 'lemon', 'bread', 'water'] as $ing) {
+	if ($soup->hasIngredient($ing)) { // $this ссылается в теле метода hasIngredient на экземпляр объекта, хранящийся в переменной $soup
+		echo "Soup conteins $ing.\n";
+	}
+	if ($sandwich->hasIngredient($ing)) {
+		echo "Sandwich conteins $ing.\n";
+	}
+}
+// Вызов статического метода
+$sizes = Entree::getSizes();
+echo "<hr>";
+//------------ КОНСТРУКТОРЫ -----------------
+// Спец метод со служебными операциями для подготовки объекта. Делаем тоже что и выше, но с использованием конструктора
+class Entree2 {
+	public $name;
+	public $ingredients = array();
+
+	public function __construct($name, $ingredients) { // метод-конструктор
+		$this->name = $name;
+		$this->ingredients = $ingredients;
+	}
+
+	public function hasIngredient($ingredient) {
+		return in_array($ingredient, $this->ingredients);
+	}
+}
+
+$soup = new Entree2('Chicken Soup', array('chicken', 'water'));
 
 ********************* МАССИВ *****************************
 
@@ -132,7 +180,7 @@ echo "<hr>";
 // ksort($array) - сортировка по ключу по алфавиту. Пара ключ-значеие также остаются.
 // rsort(), arsort(), krsort() - таже сортировка, только по убыванию.
 
-// ------- МНОГОМЕРНЫЕ МАССИВЫ ------------------
+---------------------------- МНОГОМЕРНЫЕ МАССИВЫ ------------------
 $cars = ['BMW' => ['525', '535', 'X5'], 'KIA' => ['RIO', 'CEED', 'STINGER']];
 echo $cars['KIA'][1];
 echo "<br>";
