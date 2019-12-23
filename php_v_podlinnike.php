76dc4c0b0cda49966b29afc4fa8b7606a8f46479
evgendj <workinformer@gmail.com>
2019-12-23 15:28:36 +0300

    Добавлены задания по заданию 1

--- php_v_podlinnike.php
+++ php_v_podlinnike.php
@@ -1,28 +1,55 @@
 <?php
 
 
-function findSimple(int $a, int $b) {
-  if ($a <= 1 || $a >= $b) {
-    echo "Первый аргумент должен быть больше единицы и меньше второго аргумента.";
-  } else {
-    for ($i = 0, $array = []; $a <= $b; $i++, $a++) {
-      for ($j = 2, $s = 0; $j < $a; $j++) {
-        if ($a % $j == 0) {
-          $s++;
-          break;
-        }
-      }
-    if ($s == 0) {
-      $array[] = $a;
-    }
-    }
-  }
-  return $array;
+// Даем массив и дробим на массивы, кол-во элементов кратно 3
+
+$mass = [9,2,3,6,15,2,17,9,12];
+function createTrapeze($a) {
+	for ($i = 0, $j = 0, $array1 = []; $i < count($a)/3; $i++) {
+		for($let = 'a'; $let <= 'c'; $let++, $j++) {
+			$array1[$i][$let] = $a[$j];
+		}
+	}
+	return $array1;
 }
 
-foreach (findSimple(2, 100) as $v) {
-  echo "$v ";
+function squareTrapeze($a) {
+	for($i = 0; $i < count($a); $i++) {
+		$a[$i]['s'] = 0.5 * $a[$i]['c']*($a[$i]['a'] + $a[$i]['b']);
+	}
+	return $a;
 }
+
+foreach (squareTrapeze(createTrapeze($mass)) as $key => $value) {
+  echo "Массив номер - $key<br>";
+  foreach ($value as $k => $v) {
+    echo "$k - $v<br>";
+  }
+}
+
+function getSizeForLimit($a, $b) {
+	for($i = 0, $m = 0, $array2 = []; $i < count($a); $i++) {
+		if ($a[$i]['s'] > $m && $a[$i]['s'] < $b) {
+			$array2 = $a[$i];
+		}
+	}
+	return $array2;
+}
+
+
+echo "<hr>";
+
+$mas_for_limit = squareTrapeze(createTrapeze($mass));
+
+foreach (getSizeForLimit($mas_for_limit, 17) as $key => $value) {
+  echo "$value<br>";
+}
+
+
+
+
+
+
 
 
 
