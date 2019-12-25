bcac0a48c4a56f77ee870f7444631e0ed747ba58
evgendj <workinformer@gmail.com>
2019-12-25 10:50:12 +0300

    Посмотрел второй урок добавил чуть по ООП

--- index_udemy.php
+++ index_udemy.php
@@ -1,6 +1,6 @@
 <?php
 
-// Видео 2 10 минут
+// Видео 2 просмотрел
 
 
 require_once 'classes/Car.php';
@@ -10,11 +10,26 @@ function debug($data) {
 }
 
 $car1 = new Car();
-debug($car1);
+$car2 = new Car();
+
+$car1->color = 'черный';
+$car1->brand = 'volvo';
+$car1->year = 2018;
+
+$car2->color = 'белый';
+$car2->brand = 'bmw';
+$car2->speed = 200;
+$car1->year = 2017;
+
+echo "<h3>О момем авто:</h3>
+Марка: {$car1->brand}<br>
+Цвет: {$car1->color}<br>
+Кол-во колес: {$car1->wheels}<br>
+Год выпуска: {$car1->year}<br>
+Скорость: {$car1->speed}<br>";
 
 
 
-
-
+//debug($car1);
 
  ?>
