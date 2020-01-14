c5628c53b52b1aefeb5c0d7629651f90da1ff421
evgendj <workinformer@gmail.com>
2020-01-14 17:47:44 +0300

    Продолжение работы с бд

--- connect_db.php
+++ connect_db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-  $db = new PDO('mysql:host=localhost;dbname=test_samson', 'root', '');
+  $pdo = new PDO('mysql:host=localhost;dbname=test_samson', 'root', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 } catch (PDOException $e) {
   echo "Невозможно установить соединение с базой данных";
 }
--- test.php
+++ test.php
@@ -3,3 +3,14 @@
 <?php
 
 require_once('connect_db.php');
+
+// Формируем и выполняем SQL-запрос
+
+try {
+  $query = $pdo->exec("CREATE TABLE catalogs (
+            id_catalog INT(11) NOT NULL AUTO_INCREMENT,
+            name VARCHAR(255) NOT NULL,
+            PRIMARY KEY (id_catalog))");
+} catch (PDOEXception $e) {
+  echo "Не удалось создать таблицу " . $e->getMessage();
+}
