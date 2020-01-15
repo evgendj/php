8851a0a8a6d35f91bf8f592f1f39ed1fb4b0559a
evgendj <workinformer@gmail.com>
2020-01-15 23:13:53 +0300

    Проходим вставку строк

--- forum.php
+++ forum.php
@@ -20,9 +20,7 @@ try {
 } catch (PDOEXception $e) {
   echo "Не удалось создать таблицу " . $e->getMessage();
 }
-
-try {
-  $query = $pdo->exec(
+// Трай кэтч везде будет один и тотже, только выводить сообщение нужно под соответсвующую ошибку
     "CREATE TABLE forums ( // Создание таблицы с разделами форума
       id INT(11) NOT NULL AUTO_INCREMENT,
       name TINYTEXT,
@@ -30,14 +28,8 @@ try {
       logo TINYTEXT,
       pos INT(11) DEFAULT NULL,
       hide ENUM ('show', 'hide') NOT NULL DEFAULT 'show',
-      PRIMARY KEY (id)
-    )");
-} catch (PDOEXception $e) {
-  echo "Не удалось создать таблицу " . $e->getMessage();
-}
+      PRIMARY KEY (id))"
 
-try {
-  $query = $pdo->exec(
     "CREATE TABLE themes ( // Создание таблицы с темаи разделов форума
       id INT(11) NOT NULL AUTO_INCREMENT,
       name TINYTEXT,
@@ -46,14 +38,8 @@ try {
       hide ENUM('show','hide') NOT NULL DEFAULT 'show',
       putdate DATETIME DEFAULT NULL,
       forum_id INT(11) default NULL,
-      PRIMARY KEY (id)
-    )");
-} catch (PDOEXception $e) {
-  echo "Не удалось создать таблицу " . $e->getMessage();
-}
+      PRIMARY KEY (id))"
 
-try {
-  $query = $pdo->exec(
     "CREATE TABLE posts ( // Создание таблицы с сообщениями в темах форума
       id INT(11) NOT NULL AUTO_INCREMENT,
       name TINYTEXT,
@@ -65,28 +51,50 @@ try {
       putdate DATETIME DEFAULT NULL,
       parent_post INT(11) DEFAULT NULL,
       theme_id INT(11) default NULL,
-      PRIMARY KEY (id)
-    )");
-} catch (PDOEXception $e) {
-  echo "Не удалось создать таблицу " . $e->getMessage();
-}
-// Добавление нового столбца test
-try {
-  $query = $pdo->exec("ALTER TABLE forums ADD test int(10) AFTER name");
-} catch (PDOEXception $e) {
-  echo "Не удалось создать таблицу " . $e->getMessage();}
-// Переименовывание столбца test в new_test и изменение типа поля
-try {
-  $query = $pdo->exec("ALTER TABLE forums CHANGE test new_test TEXT");
-} catch (PDOEXception $e) {
-  echo "Не удалось создать таблицу " . $e->getMessage();}
-// Изменяем только тип, при этом имя должно посторяться
-try {
-  $query = $pdo->exec("ALTER TABLE forums CHANGE new_test new_test INT(10) NOT NULL");
-} catch (PDOEXception $e) {
-  echo "Не удалось создать таблицу " . $e->getMessage();}
-// Удаление столбца new_test
-try {
-  $query = $pdo->exec("ALTER TABLE forums DROP new_test");
-} catch (PDOEXception $e) {
-  echo "Не удалось создать таблицу " . $e->getMessage();}
+      PRIMARY KEY (id))"
+
+ALTER TABLE // Работа со столбцами
+"ALTER TABLE forums ADD test int(10) AFTER name" // Добавление нового столбца test
+"ALTER TABLE forums CHANGE test new_test TEXT" // Переименовывание столбца test в new_test и изменение типа поля
+"ALTER TABLE forums CHANGE new_test new_test INT(10) NOT NULL" // Изменяем только тип, при этом имя должно посторяться
+"ALTER TABLE forums DROP new_test" // Удаление столбца new_test
+
+INSERT // Вставка строк в таблицу
+"INSERT INTO tbl VALUES (10, 20)" // Добавятся значения по порядку столбцов, при этом количество значений в скобках должно соответсвовать кол-ву столбцов в таблице.
+"INSERT INTO tbl (id_cat, id) VALUES (10, 20)" // Добавили в скобки названия столбцов, причем с измененным порядком, так и запишутся
+"INSERT INTO tbl (id) VALUES (30)" // Заполнять можно часть столбцов, остальные проставятся по умолчанию
+"INSERT INTO tbl () VALUES ()" // С пустыми скобками просто по дефолту проставятся значения. Этого же можно добиться оператором DEFAULT
+"INSERT INTO tbl (id, id_cat) VALUES (DEFAULT, DEFAULT)" // Обозначает все тоже что и в строке выше
+// Выше примеры поддерживаются всеми СУБД. Ниже только для MySQL
+"INSERT INTO tbl SET id = 40, id_cat = 50"
+"INSERT INTO tbl SET id = 50" // Можно заполнять так один столбец, остальное будет по дефолту
+"INSERT INTO catalogs VALUES (1, 'Процессоры')" // Строковые значачения вставляются в одинарные или двойные кавычки
+// если в тексте есть какие либо кавычки, то просто при вставке используй противополжные. Чет у меня не сработало. Так же можно экранировать \
+"INSERT INTO catalogs VALUES (3, 'Память \'DDR\'')" // Так получилось норм
+DATETIME DATE // это второй и третий столбец. В третий подается и дата и время, время отбросится
+"INSERT INTO tbl VALUES (1, '2016-01-03 0:00:00', '2016-01-03 0:00:00')"
+NOW() // Встроенная функция MySQL - вставка текущего времени.
+"INSERT INTO tbl VALUES (2, NOW(), NOW())"
+// В рамках одного SQL запроса время вычисляется один раз и дальше остается постоянным. Но можно использовать временные интервалы
+"INSERT INTO tbl VALUES (3, '2016-01-01 0:00:00' - INTERVAL 3 WEEk, NOW() + INTERVAL 3 MONTH)" // От первой даты отнимется 3 недели, ко второй дате прибавится 3 месяца
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
--- test.php
+++ test.php
@@ -1,4 +1,4 @@
-<!-- Скляр - 204  ***  В подлиннике - 698 (Работа в sql - 674) -->
+<!-- Скляр - 204  ***  В подлиннике - 698 (Работа в sql - 678) -->
 
 <?php
 
@@ -7,7 +7,7 @@ require_once('connect_db_forum.php');
 // Изменяем только тип, при этом имя должно посторяться
 
 try {
-  $query = $pdo->exec("ALTER TABLE forums DROP new_test");
+  $query = $pdo->exec("INSERT INTO tbl VALUES (3, '2016-01-01 0:00:00' - INTERVAL 3 WEEk, NOW() + INTERVAL 3 MONTH)");
 } catch (PDOEXception $e) {
-  echo "Не удалось создать таблицу " . $e->getMessage();
+  echo "Не удалось вставить строку " . $e->getMessage();
 }
