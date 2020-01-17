1478b2546993b8f270aec19f01c7450a59bd779a
evgendj <workinformer@gmail.com>
2020-01-17 12:02:17 +0300

    Начал SELECT изучать

--- test.php
+++ test.php
@@ -1,4 +1,4 @@
-<!-- Скляр - 208  ***  В подлиннике - 698 (Работа в sql - 683) -->
+<!-- Скляр - 215  ***  В подлиннике - 698 (Работа в sql - 686) -->
 
 <?php
 
@@ -6,9 +6,17 @@ require_once('connect_db_forum.php');
 
 // Изменяем только тип, при этом имя должно посторяться
 
+$q = $pdo->query("SELECT * FROM catalogs WHERE id > 2 AND id < 10");
+while ($row = $q->fetch()) {
+  echo "$row[0] - $row[1]<br>";
+}
+
+
+/*
 try {
   $query = $pdo->exec("UPDATE tbl SET lastdate = '2020-04-18' WHERE lastdate = '2020-04-17'");
   echo "Изменена " . $query . " строка";
 } catch (PDOEXception $e) {
   echo "Не удалось вставить строку " . $e->getMessage();
 }
+*/
