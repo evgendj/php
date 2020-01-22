47bed4ba0c4687b0ad031a9c7de413a052e99860
evgendj <workinformer@gmail.com>
2020-01-22 23:20:53 +0300

    Начал заполнять продукты

--- test.php
+++ test.php
@@ -12,16 +12,23 @@ $content = file_get_contents('product.xml');
 $product = new SimpleXMLElement($content);
 
 for ($i = 0, $cat_code = 1; $i < $product->Товар->count(); $i++, $cat_code++) {
-  foreach ($product->Товар[$i]->Разделы->Раздел as $value) {
+  foreach ($product->Товар[$i]->Разделы->Раздел as $category_name) {
     try {
-			$query = $pdo->query("SELECT * FROM a_category WHERE category_name LIKE '$value'");
+			$query = $pdo->query("SELECT * FROM a_category WHERE category_name LIKE '$category_name'");
 			$category = $query->fetch(PDO::FETCH_ASSOC);
-			if ($category['category_name'] == $value) {
-				echo "Прошло" . "<br>";
+			if ($category['category_name'] == $category_name) {
+				$category_id = $category['category_id'];
+				echo $category_id . "<br>";
 			} else {
-				$exec = $pdo->prepare("INSERT INTO a_category VALUES (NULL, $cat_code, ?, DEFAULT)");
-				$exec->execute([$value]);
+				$insert_category = $pdo->prepare("INSERT INTO a_category VALUES (NULL, $cat_code, ?, DEFAULT)");
+				$insert_category->execute([$category_name]);
+				$category_id = $pdo->lastInsertId();
+				echo $category_id . "<br>";
 			}
+			$insert_product = $pdo->prepare("INSERT INTO a_product VALUES (NULL, ?, ?, $category_id)");
+			$insert_product->execute([$product->Товар[$i]->attributes()->{'Код'}, $product->Товар[$i]->attributes()->{'Название'}]);
+			//	echo $product->Товар[$i]->attributes()->{'Код'};
+			//	echo $product->Товар[$i]->attributes()->{'Название'};
     } catch (PDOException $e) {
       echo "Ошибка загузки в базу данных" . $e->getMessage();
     }
