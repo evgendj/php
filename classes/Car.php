2b15211a800aa1886f82b40d4db8388cb170520f
evgendj <workinformer@gmail.com>
2020-02-05 23:01:15 +0300

    ООП 4. 7-55

--- classes/Car.php
+++ classes/Car.php
@@ -6,36 +6,22 @@ class Car
   public $wheels = 4;
   public $speed = 180;
   public $brand;
-  public static $countCar = 0;
 
-  const TEST_CAR = 'Прототип';
-  const TEST_CAR_SPEED = 300;
-
-
-  function __construct($color, $wheels, $speed, $brand) {
+  public function __construct($color, $wheels, $speed, $brand)
+  {
     $this->color = $color;
     $this->wheels = $wheels;
     $this->speed = $speed;
     $this->brand = $brand;
-    self::$countCar++; // Оператор разрешения области видимости (Paamayim Nekudotayim - на иврите означает двойное двоеточие)
-  }
-
-  function getCount() {
-    return self::$countCar;
   }
 
   public function getCarInfo() {
-    return "<h3>О момем авто:</h3>
+    return "<h3>О моем авто:</h3>
     Марка: {$this->brand}<br>
     Цвет: {$this->color}<br>
     Кол-во колес: {$this->wheels}<br>
+    Год выпуска: {$this->year}<br>
     Скорость: {$this->speed}<br>";
   }
 
-  public function getPrototypeInfo() {
-    return "<h3>О момем PROTOTYPE</h3>
-    Марка: " . self::TEST_CAR . "<br>
-    Скорость: " . self::TEST_CAR_SPEED . "<br>";
-  }
 }
-?>
--- test.php
+++ test.php
@@ -1,14 +1,17 @@
 <!-- Скляр - 215  ***  В подлиннике - 704 (Не записал 689-693 - Сортировка записей)-->
 <?php
-/*
+
 function debug($data) {
 	echo "<pre>" . print_r($data, 1) . "</pre>";
 }
-*/
 
-$a = 10;
+require_once('classes\Car.php');
+$car1 = new Car('black', 4, 180,'volvo');
+$car2 = new Car('white', 4, 200, 'BMW');
+//debug($car1);
 
-while ($a):
-  echo "$a<br>";
-  $a--;
-endwhile;
+//debug($car1);
+//debug($car2);
+
+echo $car1->getCarInfo();
+echo $car2->getCarInfo();
