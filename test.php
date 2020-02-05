1b93731f1b3e1b74cf43592f538e7c1807d7cc1f
evgendj <workinformer@gmail.com>
2020-02-05 11:53:24 +0300

    14 Конструкция языка 2-55

--- test.php
+++ test.php
@@ -6,11 +6,9 @@ function debug($data) {
 }
 */
 
-class SomeClass
-{
-  private $x = 100;
-}
-$a = array(1, array ("Programs hacking programs. Why?", "д'Артеньян"));
-echo "<pre>"; var_export($a); echo "</pre>";
-$obj = new SomeClass();
-echo "<pre>"; var_export($obj); echo "</pre>";
+$a = 10;
+
+while ($a):
+  echo "$a<br>";
+  $a--;
+endwhile;
