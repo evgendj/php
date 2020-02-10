ab1248cc6657b0af581f200f02cbb79e78c367dc
evgendj <workinformer@gmail.com>
2020-02-10 22:55:14 +0300

    06 12-31

--- classes/Car.php
+++ classes/Car.php
@@ -7,6 +7,42 @@ class Car
   public $speed = 180;
   public $brand;
 
+  public static $countCar = 0;
+
+  public function __construct($color, $wheels, $speed, $brand)
+  {
+    $this->color = $color;
+    $this->wheels = $wheels;
+    $this->speed = $speed;
+    $this->brand = $brand;
+    self::$countCar++;
+  }
+
+  public static function getCount() {
+
+  }
+
+  public function getCarInfo() {
+    return "<h3>О моем авто:</h3>
+    Марка: {$this->brand}<br>
+    Цвет: {$this->color}<br>
+    Кол-во колес: {$this->wheels}<br>
+    Скорость: {$this->speed}<br>";
+  }
+}
+
+
+
+
+
+/* Класс для первой части
+class Car
+{
+  public $color;
+  public $wheels = 4;
+  public $speed = 180;
+  public $brand;
+
   public function __construct($color, $wheels, $speed, $brand)
   {
     $this->color = $color;
@@ -22,5 +58,5 @@ class Car
     Кол-во колес: {$this->wheels}<br>
     Скорость: {$this->speed}<br>";
   }
-
 }
+*/
--- oop_test.php
+++ oop_test.php
@@ -5,27 +5,24 @@ function debug($data) {
 	echo "<pre>" . print_r($data, 1) . "</pre>";
 }
 
-// Класс для записи в файл
-require_once 'classes/file.php';
-
-
-
-
-
-
-
-
-
-
-/* Создание перечня характеристик автомобиля
 require_once('classes\Car.php');
+echo Car::$countCar;
 $car1 = new Car('black', 4, 180,'volvo');
+echo Car::$countCar;
 $car2 = new Car('white', 4, 200, 'BMW');
-//debug($car1);
 
-//debug($car1);
-//debug($car2);
+echo Car::$countCar;
 
 echo $car1->getCarInfo();
 echo $car2->getCarInfo();
+
+
+/*
+// Класс для записи в файл
+require_once 'classes/file.php';
+
+$file = new File(__DIR__ . '/file.txt');
+
+$file->write("Строка 1");
+$file->write("Строка 2");
 */
